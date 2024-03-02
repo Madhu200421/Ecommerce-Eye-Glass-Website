3fa638c1b0cc9e113d164b94527ccb98d0ae5acf
Madhu200421 <160255449+Madhu200421@users.noreply.github.com>
2024-03-02 20:38:38 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,6 +7,7 @@ import { Outlet } from 'react-router-dom';
 import Sidebar from '../Sidebar';
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import SearchItem from '../SearchItem';
 const Navbar = () => {
   const [side,setSide]=useState(false);
   
@@ -16,9 +17,18 @@ const Navbar = () => {
   const handleSideClose=()=>{
     setSide(false);
   }
+
+
+  
+
+ const [search,setSearch] = useState(false);
+ const handleSearch = () => {
+    search?setSearch(false):setSearch(true);
+  }
   return (
     <>
       <Sidebar sideClose={handleSideClose} side={side}/>
+      <SearchItem search={search} handleSearch={handleSearch}/>
       <nav className="navbar">
         <div className="side_icon"><FontAwesomeIcon icon={faBars} onClick={handleClick}/></div>
         <div className="navbar_logo">
@@ -27,50 +37,48 @@ const Navbar = () => {
         <div className="nav-menu">
           <ul id="navbar">
             <li>
-              <a href="/dhddj/">Home</a>
+              <a href="/">Home</a>
             </li>
 
             <li>
-              <a href="/ddj/">Eye Glasses Frames<FontAwesomeIcon icon={faCaretDown} /></a>
+              <a href="/footerpg1">Eye Glasses Frames<FontAwesomeIcon icon={faCaretDown} /></a>
               <div className="dropdown">
                 <ul>
-                  <li><a href="/footerpg1/">Men-Eye Glasses Frames</a></li>
-                  <li><a href="/footerpg2/">Women-Eye Glasses Frames</a></li>
-                  <li><a href="/Kids/">Kids-Eye Glasses Frames</a></li>
+                  <li><a href="/footerpg1">Men-Eye Glasses Frames</a></li>
+                  <li><a href="/footerpg2">Women-Eye Glasses Frames</a></li>
+                  <li><a href="/Kids">Kids-Eye Glasses Frames</a></li>
                 </ul>
               </div>
             </li>
 
             <li>
-              <a href="/jrjr/">Sun Glasses<FontAwesomeIcon icon={faCaretDown} /></a>
+              <a href="/footerpg2">Sun Glasses<FontAwesomeIcon icon={faCaretDown} /></a>
               <div className="dropdown">
                 <ul>
-                  <li><a href="/footerpg1/">Men-Eye Glasses Frames</a></li>
-                  <li><a href="/footerpg2/">Women-Eye Glasses Frames</a></li>
-                  <li><a href="/Kids/">Kids-Eye Glasses Frames</a></li>
+                  <li><a href="/footerpg1">Men-Eye Glasses Frames</a></li>
+                  <li><a href="/footerpg2">Women-Eye Glasses Frames</a></li>
+                  <li><a href="/Kids">Kids-Eye Glasses Frames</a></li>
                 </ul>
               </div>
             </li>
 
             <li>
-              <NavLink to ="/Contactlens"><a href="/ejdj/">Contact Lenses</a></NavLink>
+              <NavLink to ="/Contactlens"><a href="">Contact Lenses</a></NavLink>
             </li>
 
             <li>
-              <a href="/dkkddk/">Our Stores</a>
+              <a href="/Storelocator">Our Stores</a>
             </li>
 
             
           </ul>
           <div className="icons">
-              <FontAwesomeIcon icon={faSearch} className="icons" />
+              <FontAwesomeIcon icon={faSearch} className="icons" onClick={handleSearch}/>
               <FontAwesomeIcon icon={faHeart} className="icons" />
               <a href="/Login/"><FontAwesomeIcon icon={faUser} className="icons" /></a>
               <NavLink to="/Aboutus"><FontAwesomeIcon icon={faUsers} className="icons"/></NavLink>
               <a href="/Cart/"><FontAwesomeIcon icon={faShoppingCart} className="icons" /></a>
-              <div className="nav_cart_count" >
-                0
-              </div>
+              
           </div>
         </div>
        
