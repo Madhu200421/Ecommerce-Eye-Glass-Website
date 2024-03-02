b38f41f1a8fb2485bb1a3ce1952deb54a0891433
Madhu200421 <160255449+Madhu200421@users.noreply.github.com>
2024-03-02 21:20:45 +0530

    Add files via upload

---
+++ Lastline.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './Lastline.css';
+const Lastline=()=>{
+    return (
+        <>
+            <hr className='last'></hr>
+            <div className='wrap-text'>
+                <h6 className='lastline'>Copyright@2024TriveniEye+ | <a href="/Privacypolicy" className='privacypolicy'>Privacy Policy</a></h6>
+            </div>
+        </>
+    )
+}
+
+
+
+export default Lastline;
